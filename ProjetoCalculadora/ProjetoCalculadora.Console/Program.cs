3d4444b580ba02a2cb70e3df6c2e7924d30b10e6
Larinezen <larine.medeiros@outlook.com>
2026-03-22 09:06:09 -0300

    adicionado menu do editor de texto

--- ProjetoCalculadora/ProjetoCalculadora.Console/Program.cs
+++ ProjetoCalculadora/ProjetoCalculadora.Console/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Minha Calculadora, mais uma vez");
+﻿Console.WriteLine("-----------------------------");
+Console.WriteLine("----------CADERNO------------");
+Console.WriteLine("-----------------------------");
+
+Menu();
+
+static void Menu()
+{
+    Console.WriteLine("Menu: ");
+    Console.WriteLine("[1]- Abrir arquivo");
+    Console.WriteLine("[2]- Criar arquivo");
+    Console.WriteLine("[0]- Sair ");
+}
